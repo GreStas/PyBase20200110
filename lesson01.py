cebefffc77012f8cfed04d295e7a50b341ff34d7
GreStas <grestas2000@gmail.com>
2020-01-17 23:36:37 +0200

    lesson01 examples

--- lesson01.py
+++ lesson01.py
@@ -1,1 +1,24 @@
 print('Hello, Python!')
+printable = False
+
+is_print_in_uppercase = True
+is_print_in_local_lang = False
+
+
+def print_form(text):
+    print(text)
+
+
+class PrintInUpperCase:
+    pass
+
+
+c = PrintInUpperCase()
+
+TOTAL_PERSON_IN_CLASS = 10
+
+_my_variable = 50
+
+__my_variable = 100
+
+int_ = int(input('lalala'))
